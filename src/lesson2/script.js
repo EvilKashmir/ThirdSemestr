a3c629ee5764f1e643c49a99c7e8daec40802a20
evilkashmir.kzn@gmail.com <evilkashmir.kzn@gmail.com>
2020-09-21 10:10:11 +0300

    update html and js files

--- src/lesson2/script.js
+++ src/lesson2/script.js
@@ -1,16 +1,16 @@
 var color = ['green', 'yellow', 'red'];
-var cursor = 0;
+var index = 0;
 
 function change_color(color) {
     document.getElementById('btn-color').style.background = color;
 }
 
 function carousel() {
-    if (cursor == 3)
-        cursor = 0;
-    document.getElementById('btn-color').style.background = color[cursor];
-    setTimeout(function() {
-        cursor++;
+    if (index == 3)
+        index = 0;
+    document.getElementById('btn-color').style.background = color[index];
+    setTimeout(function () {
+        index++;
         carousel();
     }, 500);
 }
