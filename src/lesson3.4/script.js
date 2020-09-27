0d9c3406c599f75433ec11809fd09cf1d792e8ff
evilkashmir.kzn@gmail.com <evilkashmir.kzn@gmail.com>
2020-09-27 18:16:55 +0300

    add lesson6 and update lesson5/style.css

--- src/lesson3.4/script.js
+++ src/lesson3.4/script.js
@@ -2,15 +2,15 @@ color = ['green', 'yellow', 'red'];
 index = 0;
 
 function changeColor(color) {
-	document.getElementById('btn-color').style.background = color;
+    document.getElementById('btn-color').style.background = color;
 }
 
 function carousel() {
-	if (index == 3)
-		index = 0;
-	document.getElementById('btn-color').style.background = color[index];
-	setTimeout(function () {
-		index++;
-		carousel();
-	}, 500);
+    if (index == 3)
+        index = 0;
+    document.getElementById('btn-color').style.background = color[index];
+    setTimeout(function () {
+        index++;
+        carousel();
+    }, 500);
 }
